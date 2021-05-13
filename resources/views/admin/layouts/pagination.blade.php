2666a06f651201cdc405385c44ff39bef38461c8
Abolfazl <kabolfazl39@gmail.com>
2021-05-13 18:09:07 +0430

    add admin-panel layouts

---
+++ resources/views/admin/layouts/pagination.blade.php
@@ -0,0 +1,15 @@
+<nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center mt-2">
+        <li class="page-item prev-item"><a class="page-link" href="javascript:void(0);"></a></li>
+        <li class="page-item"><a class="page-link" href="javascript:void(0);">1</a></li>
+        <li class="page-item"><a class="page-link" href="javascript:void(0);">2</a></li>
+        <li class="page-item"><a class="page-link" href="javascript:void(0);">3</a></li>
+        <li class="page-item active" aria-current="page">
+            <a class="page-link" href="javascript:void(0);">4</a>
+        </li>
+        <li class="page-item"><a class="page-link" href="javascript:void(0);">5</a></li>
+        <li class="page-item"><a class="page-link" href="javascript:void(0);">6</a></li>
+        <li class="page-item"><a class="page-link" href="javascript:void(0);">7</a></li>
+        <li class="page-item next-item"><a class="page-link" href="javascript:void(0);"></a></li>
+    </ul>
+</nav>
---
+++ resources/views/admin/layouts/users-table.blade.php
@@ -0,0 +1,166 @@
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>ID</th>
+            <th></th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>1</td>
+            <th>
+                <div class="avatar">
+                    <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="avatar" width="32" height="32">
+                </div>
+            </th>
+            <td><a href="#">Peter Charls</a></td>
+            <td>Peter@Charls.com</td>
+            <td>
+                <a href="#"><span class="badge badge-pill badge-info">Admin</span></a>
+            </td>
+            <td><span class="badge badge-pill badge-primary mr-1">Enable</span></td>
+            <td>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                <a href="app-chat.html" data-toggle="tooltip" data-placement="top" title="View"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
+                <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+            </td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <th>
+                <div class="avatar">
+                    <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="avatar" width="32" height="32">
+                </div>
+            </th>
+            <td><a href="#">Peter Charls</a></td>
+            <td>Peter@Charls.com</td>
+            <td>
+                <a href="#"><span class="badge badge-pill badge-info">Admin</span></a>
+            </td>
+            <td><span class="badge badge-pill badge-primary mr-1">Enable</span></td>
+            <td>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                <a href="app-chat.html" data-toggle="tooltip" data-placement="top" title="View"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
+                <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+            </td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <th>
+                <div class="avatar">
+                    <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="avatar" width="32" height="32">
+                </div>
+            </th>
+            <td><a href="#">Peter Charls</a></td>
+            <td>Peter@Charls.com</td>
+            <td>
+                <a href="#"><span class="badge badge-pill badge-info">Admin</span></a>
+            </td>
+            <td><span class="badge badge-pill badge-primary mr-1">Enable</span></td>
+            <td>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                <a href="app-chat.html" data-toggle="tooltip" data-placement="top" title="View"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
+                <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+            </td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <th>
+                <div class="avatar">
+                    <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="avatar" width="32" height="32">
+                </div>
+            </th>
+            <td><a href="#">Peter Charls</a></td>
+            <td>Peter@Charls.com</td>
+            <td>
+                <a href="#"><span class="badge badge-pill badge-info">Admin</span></a>
+            </td>
+            <td><span class="badge badge-pill badge-primary mr-1">Enable</span></td>
+            <td>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                <a href="app-chat.html" data-toggle="tooltip" data-placement="top" title="View"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
+                <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+            </td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <th>
+                <div class="avatar">
+                    <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="avatar" width="32" height="32">
+                </div>
+            </th>
+            <td><a href="#">Peter Charls</a></td>
+            <td>Peter@Charls.com</td>
+            <td>
+                <a href="#"><span class="badge badge-pill badge-info">Admin</span></a>
+            </td>
+            <td><span class="badge badge-pill badge-primary mr-1">Enable</span></td>
+            <td>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                <a href="app-chat.html" data-toggle="tooltip" data-placement="top" title="View"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
+                <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+            </td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <th>
+                <div class="avatar">
+                    <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="avatar" width="32" height="32">
+                </div>
+            </th>
+            <td><a href="#">Peter Charls</a></td>
+            <td>Peter@Charls.com</td>
+            <td>
+                <a href="#"><span class="badge badge-pill badge-info">Admin</span></a>
+            </td>
+            <td><span class="badge badge-pill badge-primary mr-1">Enable</span></td>
+            <td>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                <a href="app-chat.html" data-toggle="tooltip" data-placement="top" title="View"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
+                <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+            </td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <th>
+                <div class="avatar">
+                    <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="avatar" width="32" height="32">
+                </div>
+            </th>
+            <td><a href="#">Peter Charls</a></td>
+            <td>Peter@Charls.com</td>
+            <td>
+                <a href="#"><span class="badge badge-pill badge-info">Admin</span></a>
+            </td>
+            <td><span class="badge badge-pill badge-primary mr-1">Enable</span></td>
+            <td>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
+                <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                <a href="app-chat.html" data-toggle="tooltip" data-placement="top" title="View"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
+                <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+            </td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
+
