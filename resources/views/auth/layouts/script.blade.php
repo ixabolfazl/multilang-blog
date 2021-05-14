64fc2e2676d1a63225a64e56ee4d1b365076e38e
Abolfazl <kabolfazl39@gmail.com>
2021-05-14 12:33:06 +0430

    change panel to admin and change rtl and ltr assets

--- resources/views/auth/layouts/script.blade.php
+++ resources/views/auth/layouts/script.blade.php
@@ -1,19 +1,15 @@
 <!-- BEGIN: Vendor JS-->
-<script src="{{ asset('assets/panel/vendors/js/vendors.min.js') }}"></script>
-<!-- BEGIN Vendor JS-->
+<script src="{{ asset('assets/admin/vendors/js/vendors.min.js') }}"></script><!-- BEGIN Vendor JS-->
 
 <!-- BEGIN: Page Vendor JS-->
-<script src="{{ asset('assets/panel/vendors/js/forms/validation/jquery.validate.min.js') }}"></script>
-@yield('vendor-script')
+<script src="{{ asset('assets/admin/vendors/js/forms/validation/jquery.validate.min.js') }}"></script>@yield('vendor-script')
 <!-- END: Page Vendor JS-->
 
 <!-- BEGIN: Theme JS-->
-<script src="{{ asset('assets/panel/js/core/app-menu.min.js') }}"></script>
-<script src="{{ asset('assets/panel/js/core/app.min.js') }}"></script>
-<!-- END: Theme JS-->
+<script src="{{ asset('assets/admin/js/core/app-menu.min.js') }}"></script>
+<script src="{{ asset('assets/admin/js/core/app.min.js') }}"></script><!-- END: Theme JS-->
 
-<!-- BEGIN: Page JS-->
-@yield('page-script')
+<!-- BEGIN: Page JS-->@yield('page-script')
 <!-- END: Page JS-->
 
 <script>
--- resources/views/auth/layouts/style.blade.php
+++ resources/views/auth/layouts/style.blade.php
@@ -1,31 +1,37 @@
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
-      rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
 <!-- BEGIN: Vendor CSS-->
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/vendors/css/vendors.min.css") }}">
-<!-- END: Vendor CSS-->
+<link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/vendors/css/vendors.min.css") }}"><!-- END: Vendor CSS-->
 
 <!-- BEGIN: Theme CSS-->
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-$lang/bootstrap.min.css") }}">
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-$lang/bootstrap-extended.min.css") }}">
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-$lang/colors.min.css") }}">
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-$lang/components.min.css") }}">
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-$lang/themes/dark-layout.min.css") }}">
-
-<!-- BEGIN: Page CSS-->
-<link rel="stylesheet" type="text/css"
-      href="{{ asset("assets/panel/css-$lang/core/menu/menu-types/vertical-menu.css") }}">
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-$lang/plugins/forms/form-validation.css") }}">
-<link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-$lang/pages/page-auth.css") }}">
-@yield('page-style')
-<!-- END: Page CSS-->
-
-<!-- BEGIN: Custom CSS-->
-@if($lang=="rtl")
-    <link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-rtl/custom-rtl.css") }}">
-    <link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-ltr/style-rtl.min.css") }}">
+@if(app()->getLocale()=='fa')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/bootstrap.min.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/bootstrap-extended.min.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/colors.min.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/components.min.css") }}">
 @else
-    <link rel="stylesheet" type="text/css" href="{{ asset("assets/panel/css-ltr/style.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/bootstrap.min.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/bootstrap-extended.min.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/colors.min.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/components.min.css") }}">
 @endif
-
+<!-- END: Theme CSS-->
+<!-- BEGIN: Page CSS-->
+@if(app()->getLocale()=='fa')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/core/menu/menu-types/vertical-menu.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/plugins/forms/form-validation.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/pages/page-auth.css") }}">
+@else
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/core/menu/menu-types/vertical-menu.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/plugins/forms/form-validation.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/pages/page-auth.css") }}">
+@endif
+<!-- END: Page CSS-->
+<!-- BEGIN: Custom CSS-->
+@if(app()->getLocale()=='fa')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/custom-rtl.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/style-rtl.min.css") }}">
+@else
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/style.css") }}">
+@endif
 <!-- END: Custom CSS-->
