a84368517e2edfbbe5e44a776c063a8dc5c02520
Abolfazl <kabolfazl39@gmail.com>
2021-05-14 12:38:20 +0430

    add author blade template

--- resources/views/app/author.blade.php
+++ resources/views/app/author.blade.php
@@ -1,1 +1,28 @@
-<?php
+@extends('app.layouts.app')
+@section('content')
+    <section class="author-area pb-40">
+        <div class="container">
+            <ul class="breadcrumb">
+                @include('app.layouts.breadcrumb')
+            </ul>
+            @include('app.layouts.author-details')
+        </div>
+    </section>
+    <section class="all-category-news ptb-40">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-md-12">
+                    <div class="section-title">
+                        <h2>همه اخبار</h2>
+                    </div>
+                    @include('app.layouts.posts-list')
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    @include('app.layouts.featured-news')
+                    @include('app.layouts.newsletter-box')
+                </div>
+            </div>
+        </div>
+    </section>
+    @include('app.layouts.more-post-area')
+@endsection
