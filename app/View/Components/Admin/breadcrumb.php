6ca7e40760fded42bd076796891b588f9259095d
Abolfazl <kabolfazl39@gmail.com>
2021-05-15 15:47:32 +0430

    complate breadcrumb

--- app/View/Components/Admin/breadcrumb.php
+++ app/View/Components/Admin/breadcrumb.php
@@ -6,14 +6,15 @@ use Illuminate\View\Component;
 
 class breadcrumb extends Component
 {
+    public $breadcrumbs;
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($breadcrumbs)
     {
-        //
+        $this->breadcrumbs = $breadcrumbs;
     }
 
     /**
--- resources/views/components/admin/breadcrumb.blade.php
+++ resources/views/components/admin/breadcrumb.blade.php
@@ -1,3 +1,20 @@
-<div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+<div class="content-header-left col-md-9 col-12 mb-2">
+    <div class="row breadcrumbs-top">
+        <div class="col-12">
+            <h2 class="content-header-title float-left mb-0">{{ array_key_last($breadcrumbs) }}</h2>
+            <div class="breadcrumb-wrapper">
+                <ol class="breadcrumb">
+                    @foreach($breadcrumbs as $breadcrumb => $route)
+                        @if($loop->last)
+                            <li class="breadcrumb-item active">{{ $breadcrumb }}</li>
+                        @else
+                            <li class="breadcrumb-item">
+                                <a href="{{ route($route) }}">{{ $breadcrumb }}</a>
+                            </li>
+                        @endif
+                    @endforeach
+                </ol>
+            </div>
+        </div>
+    </div>
 </div>
