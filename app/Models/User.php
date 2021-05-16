f9abe6b153c193ce416332426425558968211a31
Abolfazl <kabolfazl39@gmail.com>
2021-05-16 15:51:01 +0430

    add status and role to filable & add softDeletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,19 +4,20 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'phone', 'password',];
+    protected $fillable = ['name', 'email', 'phone', 'status', 'role', 'password',];
 
     /**
      * The attributes that should be hidden for arrays.
