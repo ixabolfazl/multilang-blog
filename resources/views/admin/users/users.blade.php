82f87b9dce84b64a8feaf099d9244621386c0e27
Abolfazl <kabolfazl39@gmail.com>
2021-05-17 22:29:42 +0430

    complate new users index

--- resources/views/admin/users/users.blade.php
+++ resources/views/admin/users/users.blade.php
@@ -1,4 +1,11 @@
 @extends('admin.layouts.app')
+
+@section('title','')
+
+@section('breadcrumb')
+    <x-admin.breadcrumb :breadcrumbs="$breadcrumbs"/>
+@endsection
+
 @section('content')
     <div class="content-body">
         <div class="row">
@@ -6,10 +13,12 @@
                 <div class="card">
                     <div class="card-header">
                         <h4 class="card-title">Users</h4>
-                        <a href="#newUser" class="btn btn-primary waves-effect waves-float waves-light">New User</a>
+                        <a href="{{ route('admin.users.create') }}" class="btn btn-primary waves-effect waves-float waves-light">New User</a>
                     </div>
-                    @include('admin.layouts.users-table')
-                    @include('admin.layouts.pagination')
+                    <div class="table-responsive">
+                        <x-admin.users-table :users="$users"/>
+                    </div>
+                    {{ $users->links('admin.layouts.pagination') }}
                 </div>
             </div>
         </div>
