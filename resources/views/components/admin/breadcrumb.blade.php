f1b4948652051cfcce1db86a6f91e5988d69e96a
Abolfazl <kabolfazl39@gmail.com>
2021-05-18 23:57:11 +0430

    add locals to breadcrumb.blade.php component

--- resources/views/components/admin/breadcrumb.blade.php
+++ resources/views/components/admin/breadcrumb.blade.php
@@ -1,15 +1,15 @@
 <div class="content-header-left col-md-9 col-12 mb-2">
     <div class="row breadcrumbs-top">
         <div class="col-12">
-            <h2 class="content-header-title float-left mb-0">{{ array_key_last($breadcrumbs) }}</h2>
+            <h2 class="content-header-title float-left mb-0">{{ __(array_key_last($breadcrumbs)) }}</h2>
             <div class="breadcrumb-wrapper">
                 <ol class="breadcrumb">
                     @foreach($breadcrumbs as $breadcrumb => $route)
                         @if($loop->last)
-                            <li class="breadcrumb-item active">{{ $breadcrumb }}</li>
+                            <li class="breadcrumb-item active">{{ __($breadcrumb) }}</li>
                         @else
                             <li class="breadcrumb-item">
-                                <a href="{{ route($route) }}">{{ $breadcrumb }}</a>
+                                <a href="{{ route($route) }}">{{ __($breadcrumb) }}</a>
                             </li>
                         @endif
                     @endforeach
