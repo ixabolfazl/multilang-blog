038e97462120974628ebe9d9394c889259019451
Abolfazl <kabolfazl39@gmail.com>
2021-05-19 12:44:43 +0430

    add sweetalert and localsr to users/deleted.blade.php

--- resources/views/admin/users/deleted.blade.php
+++ resources/views/admin/users/deleted.blade.php
@@ -1,5 +1,13 @@
 @extends('admin.layouts.app')
-
+@section('vendor-style')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/vendors/css/extensions/sweetalert2.min.css") }}">
+@endsection
+@section('page-style-rtl')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/plugins/extensions/ext-component-sweet-alerts.css") }}">
+@endsection
+@section('page-style-ltr')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/plugins/extensions/ext-component-sweet-alerts.css") }}">
+@endsection
 @section('title','')
 
 @section('breadcrumb')
@@ -12,7 +20,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title">Deleted Users</h4>
+                        <h4 class="card-title">{{__('Deleted Users')}}</h4>
                     </div>
                     <div class="table-responsive">
                         <x-admin.users-table :users="$users" type="deleted"/>
@@ -22,4 +30,22 @@
             </div>
         </div>
     </div>
+@endsection
+@section('vendor-script')
+    <script src="{{ asset('assets/admin/vendors/js/extensions/sweetalert2.all.min.js') }}"></script>
+@endsection
+@section('page-script')
+    <script src="{{ asset('assets/admin/js/scripts/extensions/ext-component-sweet-alerts.js') }}"></script>
+    @if(Session::has('status'))
+        <script>
+            $(window).on('load', function () {
+                Swal.fire({
+                    icon: 'success',
+                    title: '{{ Session::get('status') }}',
+                    showConfirmButton: false,
+                    timer: 3000,
+                });
+            })
+        </script>
+    @endif
 @endsection
