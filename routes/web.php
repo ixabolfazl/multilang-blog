bffd2ce3e1bc9f9a40a0596008c73d81afa45375
Abolfazl <kabolfazl39@gmail.com>
2021-05-23 23:20:31 +0430

    add categories routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
@@ -39,6 +40,7 @@ Route::localized(function () {
             Route::get('{id}/restore', [UserController::class, 'restore'])->name('restore');
         });
         Route::resource('users', UserController::class);
+        Route::resource('categories', CategoryController::class);
 
 
     });
