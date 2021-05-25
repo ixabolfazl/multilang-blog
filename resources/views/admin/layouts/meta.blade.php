29189a61a816265d627425c9adbe1d7603f5266f
Abolfazl <kabolfazl39@gmail.com>
2021-05-25 23:25:37 +0430

    add site name to title admin/meta

--- resources/views/admin/layouts/meta.blade.php
+++ resources/views/admin/layouts/meta.blade.php
@@ -1,6 +1,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-<title>@yield('title')</title>
+<title> سایت | @yield('title')</title>
 <link rel="apple-touch-icon" href="{{ asset('assets/admin/images/ico/apple-icon-120.png') }}">
 <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/admin/images/ico/favicon.ico') }}">@include('admin.layouts.style')
