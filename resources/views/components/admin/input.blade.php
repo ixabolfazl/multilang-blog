71bf4d7149149a63e21d6779f282065f6897481d
Abolfazl <kabolfazl39@gmail.com>
2021-05-27 12:43:53 +0430

    fix errors in input component

--- resources/views/components/admin/input.blade.php
+++ resources/views/components/admin/input.blade.php
@@ -5,16 +5,16 @@
     </div>
     @if($type=='file')
         <div class="custom-file">
-            <input type="file" class="custom-file-input @error($name) is-invalid @enderror" id="{{ $name }}" name="{{ $name }}" tabindex="{{ $tabindex }}">
+            <input type="file" class="custom-file-input @error(str_replace("[",".",trim($name,"]"))) is-invalid @enderror" id="{{ $name }}" name="{{ $name }}" tabindex="{{ $tabindex }}">
             <label class="custom-file-label" for="{{ $name }}">{{ $placeholder }}</label>
             @error($name)
             <div class="invalid-feedback">{{ $message }}</div>
             @enderror
         </div>
     @else
-        <input type="{{ $type }}" class="form-control @error($name) is-invalid @enderror" id="{{ $name }}" name="{{ $name }}" placeholder="{{ $placeholder }}" aria-describedby="{{ $name }}" tabindex="{{ $tabindex }}" autofocus value="{{ $value == null || $errors->any() ? old($name) : $value }}"/>
+        <input type="{{ $type }}" class="form-control @error(str_replace("[",".",trim($name,"]"))) is-invalid @enderror" id="{{ $name }}" name="{{ $name }}" placeholder="{{ $placeholder }}" aria-describedby="{{ $name }}" tabindex="{{ $tabindex }}" autofocus value="{{ $value == null || $errors->any() ? old(str_replace("[",".",trim($name,"]"))) : $value }}"/>
     @endif
-    @error($name)
+    @error(str_replace("[",".",trim($name,"]")))
     <div class="invalid-feedback">{{ $message }}</div>
     @enderror
 </div>
