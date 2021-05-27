2cffc0abbba63a319dd01aa831408649f5f0a204
Abolfazl <kabolfazl39@gmail.com>
2021-05-27 23:09:06 +0430

    add addCategoryRequest

---
+++ app/Http/Requests/Admin/AddCategoryReqest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Astrotomic\Translatable\Validation\RuleFactory;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddCategoryReqest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $locals = (config('translatable.locales'));
+        $mainLocal = array_shift($locals);
+
+        $mainRule = [
+            'slug' => 'required|regex:/^[a-z0-9-]+$/|unique:categories'
+            , "$mainLocal.name" => 'required|string',
+            "$mainLocal.meta" => ['nullable', "required_with:$mainLocal.name", 'string'],
+            'category_id' => 'nullable|numeric'
+        ];
+        $ruleFactory = RuleFactory::make([
+            '%name%' => 'nullable|string',
+            '%meta%' => ['nullable', 'required_with:%name%', 'string'],
+        ], null, null, null, $locals);
+
+        return array_merge($mainRule, $ruleFactory);
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return RuleFactory::make([
+            '%name%' => __('Name'),
+            '%meta%' => __('Meta'),
+        ]);
+    }
+}
