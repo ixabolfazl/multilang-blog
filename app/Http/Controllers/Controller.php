9480327ea0b7023e005d02b8996a7e3723a71af4
Abolfazl <kabolfazl39@gmail.com>
2021-05-28 11:08:24 +0430

    add removeNullFields method

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    /**
+     * remove null fields in languages.
+     * @param Request $request
+     * @return Request
+     */
+    public function removeNullFields(Request $request)
+    {
+        $request = $request->all();
+        foreach ($request as $inputs => $value) {
+            if (is_array($value)) {
+                foreach ($value as $items => $item) {
+                    if ($item == null)
+                        unset($request[$inputs][$items]);
+
+                }
+            }
+        }
+        $newRequest = new Request();
+        $newRequest->initialize(array_filter($request));
+        return $newRequest;
+    }
+
 }
