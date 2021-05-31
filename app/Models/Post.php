f6b18630ccd5c01380982961b07c3779205b5049
Abolfazl <kabolfazl39@gmail.com>
2021-05-31 12:46:03 +0430

    add Date Attribute to Post-Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Astrotomic\Translatable\Translatable;
+use Hekmatinasser\Verta\Facades\Verta;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory, Translatable;
+    use HasFactory, Translatable, softDeletes;
 
     public $translatedAttributes = ['title', 'description', 'body', 'meta', 'keywords'];
     protected $fillable = ['slug', 'status', 'user_id', 'image',];
@@ -31,4 +33,14 @@ class Post extends Model
     {
         return is_null($this->user) ? null : $this->user->name;
     }
+
+    /**
+     * Return local created_at post.
+     *
+     */
+    public function getDateAttribute()
+    {
+        return app()->getLocale() == 'fa' ? Verta::instance($this->created_at)->format('Y/m/d') : ($this->created_at)->format('Y/m/d');
+    }
+
 }
