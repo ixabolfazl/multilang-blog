024152e8d86b509e9a1a8255f6245842d66a3382
Abolfazl <kabolfazl39@gmail.com>
2021-06-05 22:55:27 +0430

    add old-value to textarea content

--- resources/views/components/admin/textarea.blade.php
+++ resources/views/components/admin/textarea.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}</label>
-    <textarea dir="{{ $dir }}" id="{{ $name }}" name="{{ $name }}" class="form-control {{$type =='editor'?'editor':''}}" placeholder="{{ $placeholder }}" rows="{{ $rows }}" tabindex="{{ $tabindex }}">{{ $value }}</textarea>
+    <textarea dir="{{ $dir }}" id="{{ $name }}" name="{{ $name }}" class="form-control {{$type =='editor'?'editor':''}}" placeholder="{{ $placeholder }}" rows="{{ $rows }}" tabindex="{{ $tabindex }}">{{ $value == null || $errors->any() ? old(str_replace("[",".",trim($name,"]"))) : $value }}</textarea>
 </div>
