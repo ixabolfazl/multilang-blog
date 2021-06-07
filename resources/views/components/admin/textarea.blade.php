589238d02698284e6f59b378ea5506dd83db0d7b
Abolfazl <kabolfazl39@gmail.com>
2021-06-07 23:58:47 +0430

    fix value in textarea component

--- resources/views/components/admin/textarea.blade.php
+++ resources/views/components/admin/textarea.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="{{ $name }}">{{ $label }}</label>
-    <textarea dir="{{ $dir }}" id="{{ $name }}" name="{{ $name }}" class="form-control {{$type =='editor'?'editor':''}} @error(str_replace("[",".",trim($name,"]"))) is-invalid @enderror" placeholder="{{ $placeholder }}" rows="{{ $rows }}" tabindex="{{ $tabindex }}">{{ $value == null || $errors->any() ? old(str_replace("[",".",trim($name,"]"))) : $value }}</textarea>
+    <textarea dir="{{ $dir }}" id="{{ $name }}" name="{{ $name }}" class="form-control {{$type =='editor'?'editor':''}} @error(str_replace("[",".",trim($name,"]"))) is-invalid @enderror" placeholder="{{ $placeholder }}" rows="{{ $rows }}" tabindex="{{ $tabindex }}">{!!   $value == null || $errors->any() ? old(str_replace("[",".",trim($name,"]"))) : $value !!}</textarea>
     @error(str_replace("[",".",trim($name,"]")))
     <div class="invalid-feedback">{{ $message }}</div>
     @enderror
