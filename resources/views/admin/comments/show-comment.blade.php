f80440488e879f8d83da969680b2219243a5c9c9
Abolfazl <kabolfazl39@gmail.com>
2021-06-14 23:37:39 +0430

    complate show-comment view

--- resources/views/admin/comments/show-comment.blade.php
+++ resources/views/admin/comments/show-comment.blade.php
@@ -1,4 +1,17 @@
 @extends('admin.layouts.app')
+@section('vendor-style')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/vendors/css/extensions/sweetalert2.min.css") }}">
+@endsection
+@section('page-style-rtl')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-rtl/plugins/extensions/ext-component-sweet-alerts.css") }}">
+@endsection
+@section('page-style-ltr')
+    <link rel="stylesheet" type="text/css" href="{{ asset("assets/admin/css-ltr/plugins/extensions/ext-component-sweet-alerts.css") }}">
+@endsection
+@section('title',__(array_key_last($breadcrumbs)))
+@section('breadcrumb')
+    <x-admin.breadcrumb :breadcrumbs="$breadcrumbs"/>
+@endsection
 @section('content')
     <div class="content-body">
         <div class="row">
@@ -6,64 +19,84 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="d-flex justify-content-start align-items-center mb-1">
-                            <!-- avatar -->
-                            <div class="avatar mr-1">
-                                <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-1.jpg') }}" alt="avatar img" height="50" width="50"/>
-                            </div>
-                            <!--/ avatar -->
+                            @if(is_null($comment->user->image))
+                                <div class="avatar bg-{{ ['primary','danger','warning','success','info','secondary'][$comment->user->id%6] }}">
+                                    <div class="avatar-content">{{ substr($comment->user->email,0,2) }}</div>
+                                </div>
+                            @else
+                                <div class="avatar mr-1">
+                                    <img src="{{ asset($comment->user->image) }}" alt="avatar" width="50" height="50">
+                                </div>
+                            @endif
                             <div class="col-lg-10 col-12">
                                 <div class="profile-user-info">
-                                    <h6 class="mb-0">Leeanna Alvord</h6>
-                                    <small class="text-muted">12 Dec 2018 at 1:16 AM</small>
+                                    <h6 class="mb-0">{{ $comment->user->name }}</h6>
+                                    <small class="text-muted">{{ $comment->date  }}</small>
                                 </div>
                             </div>
                             <div class="col-lg-2 col-12">
                                 <div class="align-items-center">
-                                    <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
-                                    <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
-                                    <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
-                                    <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+                                    <form action="{{ route('admin.comments.destroy',$comment->id) }}" id="destroy-comment-{{$comment->id}}" method="post">
+                                        <a href="{{ route('admin.comments.status',$comment->id) }}" data-toggle="tooltip" data-placement="top" title="{{__($comment->is_approved ? 'Unapprove' :'Approve')}}"><span class="badge badge-pill badge-{{$comment->is_approved?'warning':'success'}}"><i data-feather='{{$comment->is_approved?'eye-off':'check'}}'></i></span></a>
+                                        <a href="{{ route('admin.comments.edit',$comment->id) }}" data-toggle="tooltip" data-placement="top" title="{{ __('Edit') }}"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
+                                        @csrf
+                                        @method('DELETE')
+                                        <a href="{{ route('admin.comments.destroy',$comment->id) }}" onclick="destroyComment({{ $comment->id }})" data-toggle="tooltip" data-placement="top" title="{{ __('Delete') }}"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
+                                    </form>
                                 </div>
                             </div>
 
                         </div>
-                        <p class="card-text">
-                            Wonderful Machine· A well-written bio allows viewers to get to know a photographer beyond the work. This can make the difference when presenting to clients who are looking for the perfect fit. </p>
+                        <p class="card-text">{{ $comment->comment }} </p>
                         <hr>
-                        <div class="d-flex align-items-start mb-1">
-                            <div class="avatar mt-25 mr-75">
-                                <img src="{{ asset('assets/admin/images/portrait/small/avatar-s-19.jpg') }}" alt="Avatar" height="34" width="34"/>
-                            </div>
-                            <div class="profile-user-info w-100">
-                                <div class="d-flex align-items-center justify-content-between">
-                                    <h6 class="mb-0">Kitty Allanson</h6>
-                                </div>
-
-                                <small>Wonderful Machine· A well-written bio allows viewers to get to know a photographer beyond the work. This can make the difference when presenting to clients who are looking for the perfect fit.</small>
-                            </div>
-                            <div class="col-lg-2 col-12">
-                                <div class="align-items-center">
-                                    <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Enable"><span class="badge badge-pill badge-success"><i data-feather='check'></i></span></a>
-                                    <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Disable"><span class="badge badge-pill badge-warning"><i data-feather='eye-off'></i></span></a>
-                                    <a href="app-email.html" data-toggle="tooltip" data-placement="top" title="Edit"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
-                                    <a href="app-todo.html" data-toggle="tooltip" data-placement="top" title="Delete"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <form action="">
-                            <fieldset class="form-label-group mb-75">
-                                <textarea class="form-control" id="textarea" name="comment" rows="3" placeholder="Add Replay"></textarea>
-                                <label for="textarea">Replay Comment</label>
-                            </fieldset>
-                            <input type="submit" class="btn btn-sm btn-primary" value="Replay">
+                        <form action="{{ route('admin.comments.store',$comment->id) }}" method="post">
+                            @csrf
+                            <x-admin.textarea name="comment" :label="__('Replay')" tabindex="1" rows="3"/>
+                            <input type="submit" class="btn btn-sm btn-primary" value="{{ __('Replay') }}">
                         </form>
-
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
+@endsection
+@section('vendor-script')
+    <script src="{{ asset('assets/admin/vendors/js/extensions/sweetalert2.all.min.js') }}"></script>
+@endsection
+@section('page-script')
+    @if(Session::has('status'))
+        <script>
+            $(window).on('load', function () {
+                Swal.fire({
+                    icon: 'success',
+                    title: '{{__('Success')}}',
+                    text: '{{ Session::get('status') }}',
+                    showConfirmButton: false,
+                    timer: 3000,
+                });
+            })
+        </script>
+    @endif
+    <script>
+        function destroyComment(id) {
+            event.preventDefault();
+            Swal.fire({
+                title: '{{ __('Are you sure?')}}',
+                text: '{{__('You wont be able to revert this!')}}',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: '{{ __('Yes, delete it!')}}',
+                cancelButtonText: '{{ __('Cancel')}}',
+                customClass: {
+                    confirmButton: 'btn btn-primary',
+                    cancelButton: 'btn btn-outline-danger ml-1'
+                },
+                buttonsStyling: false
+            }).then(function (result) {
+                if (result.value) {
+                    document.getElementById(`destroy-comment-${id}`).submit();
+                }
+            });
+        }
+    </script>
 @endsection
