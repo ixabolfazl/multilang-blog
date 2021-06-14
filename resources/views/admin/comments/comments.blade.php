4cc2c17f30d7f7a3f55cd5ecd95740c8787245c1
Abolfazl <kabolfazl39@gmail.com>
2021-06-14 23:34:55 +0430

    add date to comments view

--- resources/views/admin/comments/comments.blade.php
+++ resources/views/admin/comments/comments.blade.php
@@ -29,6 +29,7 @@
                                 <th>{{__('For')}}</th>
                                 <th>{{__('User')}}</th>
                                 <th>{{__('Status')}}</th>
+                                <th>{{__('Date')}}</th>
                                 <th>{{__('Replies')}}</th>
                                 <th>{{__('Actions')}}</th>
                             </tr>
@@ -37,7 +38,7 @@
                             @foreach($comments as $comment)
                                 <tr>
                                     <td>{{ $loop->index+1 }}</td>
-                                    <td>{{ Str::limit($comment->content, 50) }}</td>
+                                    <td>{{ Str::limit($comment->comment, 50) }}</td>
                                     <td>
                                         <a href="{{ route('admin.users.edit',$comment->post->id)}}">{{ Str::limit($comment->post->title, 50) }}</a>
                                     </td>
@@ -47,16 +48,17 @@
                                     <td>
                                         <span class="badge badge-pill badge-{{ $comment->is_approved ? 'success' : 'danger'}} mr-1">{{ __($comment->is_approved ? 'Approved' : 'Not approved') }}</span>
                                     </td>
+                                    <td>{{ $comment->date }}</td>
                                     <td>
                                         {{ $comment->replies_count }}
                                     </td>
                                     <td>
                                         <form action="{{ route('admin.comments.destroy',$comment->id) }}" id="destroy-comment-{{$comment->id}}" method="post">
+                                            <a href="{{ route('admin.comments.show',$comment->id) }}" data-toggle="tooltip" data-placement="top" title="{{ __('View & Reply') }}"><span class="badge badge-pill badge-info"><i data-feather='message-square'></i></span></a>
                                             <a href="{{ route('admin.comments.status',$comment->id) }}" data-toggle="tooltip" data-placement="top" title="{{__($comment->is_approved ? 'Unapprove' :'Approve')}}"><span class="badge badge-pill badge-warning"><i data-feather='{{$comment->is_approved?'eye-off':'check'}}'></i></span></a>
                                             <a href="{{ route('admin.comments.edit',$comment->id) }}" data-toggle="tooltip" data-placement="top" title="{{ __('Edit') }}"><span class="badge badge-pill badge-secondary"><i data-feather="edit"></i></span></a>
                                             @csrf
                                             @method('DELETE')
-                                            <a href="{{ route('admin.comments.show',$comment->id) }}" data-toggle="tooltip" data-placement="top" title="{{ __('View') }}"><span class="badge badge-pill badge-info"><i data-feather='external-link'></i></span></a>
                                             <a href="{{ route('admin.comments.destroy',$comment->id) }}" onclick="destroyComment({{ $comment->id }})" data-toggle="tooltip" data-placement="top" title="{{ __('Delete') }}"><span class="badge badge-pill badge-danger"><i data-feather="trash-2"></i></span></a>
                                         </form>
                                     </td>
