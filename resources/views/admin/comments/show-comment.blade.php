b0af4e6a4712e0b582eca7c31f87f367e9f737ac
Abolfazl <kabolfazl39@gmail.com>
2021-06-15 18:06:34 +0430

    add post details to show-comment view

--- resources/views/admin/comments/show-comment.blade.php
+++ resources/views/admin/comments/show-comment.blade.php
@@ -18,6 +18,20 @@
             <div class="col-lg-12 col-12 order-1 order-lg-2">
                 <div class="card">
                     <div class="card-body">
+                        <div class="employee-task d-flex justify-content-between align-items-center  mb-1">
+                            <div class="media">
+                                <div class="avatar mr-75">
+                                    <img src="{{ asset($comment->post->image) }}" class="rounded" width="50" height="50" alt="Avatar">
+                                </div>
+                                <div class="media-body my-auto">
+                                    <h5 class="mb-0">
+                                        <a href="{{ route('admin.posts.edit',$comment->post->id) }}">{{ $comment->post->title }}</a>
+                                    </h5>
+                                    <small>{{ Str::limit($comment->post->description, 100) }}</small>
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
                         <div class="d-flex justify-content-start align-items-center mb-1">
                             @if(is_null($comment->user->image))
                                 <div class="avatar bg-{{ ['primary','danger','warning','success','info','secondary'][$comment->user->id%6] }}">
@@ -25,7 +39,7 @@
                                 </div>
                             @else
                                 <div class="avatar mr-1">
-                                    <img src="{{ asset($comment->user->image) }}" alt="avatar" width="50" height="50">
+                                    <img src="{{ asset($comment->user->image) }}" alt="avatar" width="42" height="42">
                                 </div>
                             @endif
                             <div class="col-lg-10 col-12">
