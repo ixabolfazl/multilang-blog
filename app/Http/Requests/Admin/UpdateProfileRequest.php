12136efbdda97e1cf9e12d5ecdd3e9730788ceda
Abolfazl <kabolfazl39@gmail.com>
2021-07-08 21:51:59 +0430

    add UpdateProfileRequest

---
+++ app/Http/Requests/Admin/UpdateProfileRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:50',
+            'phone' => ['required', 'numeric', Rule::unique('users')->ignore(auth()->user()->id)],
+            'image' => 'nullable|image|max:1024',
+            'password' => 'nullable|string|confirmed|min:8',
+        ];
+    }
+}
