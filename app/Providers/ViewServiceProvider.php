458ead6c6176b29079faaa7f71cb6c36c61cb12a
Abolfazl <kabolfazl39@gmail.com>
2021-07-25 12:16:19 +0430

    add ViewServiceProvider

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Setting;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // settings
+        View::share('setting', Setting::where('lang', app()->getLocale())->with('breaking_title_category')->get());
+    }
+}
