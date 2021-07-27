cdf43f4e9263c2a993b8ddd058d25bb3d7afe1c7
Abolfazl <kabolfazl39@gmail.com>
2021-07-27 23:18:50 +0430

    complate SettingController's methods

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -3,35 +3,52 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\SettingRequest;
+use App\Models\Category;
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class SettingController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function index()
     {
-        $settings = Setting::all()->mapWithKeys(function ($item) {
-            return [$item['key'] => $item['value']];
-        });
-        dd($settings->all());
-
-        //
+//        $settings = Setting::all()->mapWithKeys(function ($item) {
+//            return [$item['key'] => $item['value']];
+//        });
+//        dd($settings->all());
+        $breadcrumbs = ['Dashboard' => 'admin.dashboard', 'Setting' => '',];
+        $categories = Category::all();
+        return view('admin.setting', compact(['categories', 'breadcrumbs']));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param \App\Models\Setting $setting
-     * @return \Illuminate\Http\Response
+     * @param SettingRequest $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, Setting $setting)
+    public function update(SettingRequest $request)
     {
-        //
+        $locals = (config('translatable.locales'));
+        foreach ($locals as $local) {
+
+            $setting = Setting::where('lang', $local)->first();
+
+            if ($request->hasFile($local . '.logo_url') && $request->file($local . '.logo_url')->isValid()) {
+                $ext = $request->file($local . '.logo_url')->getClientOriginalExtension();
+                File::delete($setting->logo_url);
+                $request->file($local . '.logo_url')->move(public_path('assets/app/img/'), 'logo-' . $local . '.' . $ext);
+                $logo = 'assets/app/img/logo-' . $local . '.' . $ext;
+            }
+
+            $setting->update(array_merge(array_filter($request->$local), ['logo_url' => $logo ?? $setting->logo_url]));
+        }
+        return redirect()->back()->with('status', __('The setting was :atrribute successfully!', ['atrribute' => __('updated')]));
     }
 }
