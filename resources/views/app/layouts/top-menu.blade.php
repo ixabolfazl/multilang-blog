a924472eaf0656b3faefa56bf9f77eead6a6e613
Abolfazl <kabolfazl39@gmail.com>
2021-08-03 15:51:34 +0430

    complate app/topmenu

--- resources/views/app/layouts/top-menu.blade.php
+++ resources/views/app/layouts/top-menu.blade.php
@@ -1,49 +1,23 @@
 <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
     <ul class="navbar-nav">
-        <li class="nav-item"><a href="{{ route('home') }}" class="nav-link active">{{ __('Home') }}</a>
-        </li>
-        <li class="nav-item"><a href="post-category-2.html" class="nav-link">سبک زندگی</a></li>
-        <li class="nav-item"><a href="post-category-3.html" class="nav-link">مسافرت</a></li>
-        <li class="nav-item"><a href="index.htm#" class="nav-link">صفحات</a>
-            <ul class="dropdown-menu">
-                <li class="nav-item"><a href="index.htm#" class="nav-link">کلاس دسته بندی</a>
+        <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">{{ __('Home') }}</a></li>
+        @foreach($categories as $category)
+            <li class="nav-item">
+                <a href="{{ route('category.show',$category->slug) }}" class="nav-link">{{ $category->name }}</a>
+                @if($category->children()->count()>0)
                     <ul class="dropdown-menu">
-                        <li class="nav-item"><a href="post-category-1.html" class="nav-link">دسته بندی 1</a></li>
-                        <li class="nav-item"><a href="post-category-2.html" class="nav-link">دسته بندی 2</a></li>
-                        <li class="nav-item"><a href="post-category-3.html" class="nav-link">دسته بندی 3</a></li>
-                        <li class="nav-item"><a href="post-category-4.html" class="nav-link">دسته بندی 4</a></li>
-                        <li class="nav-item"><a href="post-category-5.html" class="nav-link">دسته بندی 5</a></li>
-                        <li class="nav-item"><a href="post-category-6.html" class="nav-link">دسته بندی 6</a></li>
+                        @foreach($category->children as $subCategory)
+                            <li class="nav-item">
+                                <a href="{{ route('category.show',$subCategory->slug) }}" class="nav-link">{{ $subCategory->name }}</a>
+                            </li>
+                        @endforeach
                     </ul>
-                </li>
-                <li class="nav-item"><a href="index.htm#" class="nav-link">کلاس پست ها</a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-item"><a href="single-post-1.html" class="nav-link">پست جداگانه 1</a></li>
-                        <li class="nav-item"><a href="single-post-2.html" class="nav-link">پست جداگانه 2</a></li>
-                        <li class="nav-item"><a href="single-post-3.html" class="nav-link">پست جداگانه 3</a></li>
-                        <li class="nav-item"><a href="single-post-4.html" class="nav-link">پست جداگانه 4</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item"><a href="index.htm#" class="nav-link">نمایشگاه</a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-item"><a href="gallery-1.html" class="nav-link">گالری 1</a>
-                        </li>
-                        <li class="nav-item"><a href="gallery-2.html" class="nav-link">گالری 2</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="nav-item"><a href="author.html" class="nav-link">نویسنده</a></li>
-                <li class="nav-item"><a href="contact.html" class="nav-link">تماس با ما</a></li>
-                <li class="nav-item"><a href="login.html" class="nav-link">ورود</a></li>
-                <li class="nav-item"><a href="signup.html" class="nav-link">ثبت نام</a></li>
-                <li class="nav-item"><a href="404-error.html" class="nav-link">خطای 404</a></li>
-                <li class="nav-item"><a href="coming-soon.html" class="nav-link">نسخه به زودی</a>
-                </li>
-            </ul>
-        </li>
+                @endif
+            </li>
+        @endforeach
         @auth
             <li class="nav-item">
-                <a href="m#" class="nav-link"><i class="icofont-user-alt-5"></i> {{ auth()->user()->name }}</a>
+                <a href="#" class="nav-link"><i class="icofont-user-alt-5"></i> {{ auth()->user()->name }}</a>
                 <ul class="dropdown-menu">
                     @can('viewAny','dashboard')
                         <li class="nav-item">
@@ -86,8 +60,6 @@
                 @endforeach
             </ul>
         </li>
-
-
     </ul>
     <div class="others-options">
         <ul>
@@ -99,7 +71,7 @@
                     <form>
                         <span class="nav-search-close-button" tabindex="0">✕</span>
                         <div class="nav-search-inner">
-                            <input type="text" name="search" placeholder="جستجو کنید ....">
+                            <input type="text" name="search" placeholder="{{__('Search')}}">
                         </div>
                     </form>
                 </div>
