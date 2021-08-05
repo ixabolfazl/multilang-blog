59a7889e2972934ab531c362d4c079efde04b994
Abolfazl <kabolfazl39@gmail.com>
2021-08-05 17:20:56 +0430

    complate category view

--- resources/views/app/category.blade.php
+++ resources/views/app/category.blade.php
@@ -1,4 +1,7 @@
-@extends('app.layouts.app')
+@extends('app.layouts.app')@section('title',$category->name)
+@section('seo')
+    {!! SEO::generate() !!}
+@endsection
 @section('content')
     <div class="page-title-area">
         <div class="container">
@@ -7,27 +10,18 @@
             </ul>
         </div>
     </div>
-
     @include('app.layouts.catrgory-top-post')
-
     <section class="all-category-news ptb-40">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-md-12">
 
                     <div class="section-title">
-                        <h2>همه اخبار</h2>
+                        <h2>{{ $category->name }}</h2>
                     </div>
                     @include('app.layouts.posts-list')
-
-                </div>
-                <div class="col-lg-4 col-md-12">
-
-                    @include('app.layouts.featured-news')
-                    @include('app.layouts.newsletter-box')
                 </div>
             </div>
         </div>
     </section>
-    @include('app.layouts.more-post-area')
 @endsection
