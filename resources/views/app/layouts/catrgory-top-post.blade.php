46132ab91aa68d7e02fb5578f7ee246eb764b0e3
Abolfazl <kabolfazl39@gmail.com>
2021-08-05 17:21:32 +0430

    complate category-top-post view

--- resources/views/app/layouts/catrgory-top-post.blade.php
+++ resources/views/app/layouts/catrgory-top-post.blade.php
@@ -2,26 +2,27 @@
     <div class="container">
         <div class="row">
             <div class="default-news-slides owl-carousel owl-theme">
-                <div class="col-lg-12 col-md-12">
-                    <div class="single-default-news">
-                        @include('app.layouts.single-post-content')
+                @foreach($topPosts as $topPost)
+                    <div class="col-lg-12 col-md-12">
+                        <div class="single-default-news">
+                            <img src="{{ asset($topPost->image) }}" alt="image">
+                            <div class="news-content">
+                                <ul>
+                                    <li>
+                                        <i class="icofont-user-suited"></i><a href="{{ route('user.show',$topPost->user->id) }}">{{ $topPost->user->name }}</a>
+                                    </li>
+                                    <li><i class="icofont-calendar"></i>{{ $topPost->date }}</li>
+                                </ul>
+                                <h3><a href="{{ route('post.show',$topPost->slug) }}">{{ $topPost->title }}</a></h3>
+                            </div>
+                            <div class="tags bg-{{ $topPost->id%4+1 }}">
+                                @foreach($topPost->categories as $topPostCategories)
+                                    <a href="{{ route('category.show',$topPostCategories->slug) }}">{{ $topPostCategories->name }}</a>
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-12 col-md-12">
-                    <div class="single-default-news">
-                        @include('app.layouts.single-post-content')
-                    </div>
-                </div>
-                <div class="col-lg-12 col-md-12">
-                    <div class="single-default-news">
-                        @include('app.layouts.single-post-content')
-                    </div>
-                </div>
-                <div class="col-lg-12 col-md-12">
-                    <div class="single-default-news">
-                        @include('app.layouts.single-post-content')
-                    </div>
-                </div>
+                @endforeach
             </div>
         </div>
     </div>
