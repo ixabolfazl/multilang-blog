869dd0007bfe6d12cef973fa7075e1c8b9369da6
Abolfazl <kabolfazl39@gmail.com>
2021-08-05 17:22:03 +0430

    complate pagination view

--- resources/views/app/layouts/pagination.blade.php
+++ resources/views/app/layouts/pagination.blade.php
@@ -1,8 +1,33 @@
-<div class="pagination-area">
-    <a href="index.htm#" class="prev page-numbers"><i class="icofont-double-right"></i></a>
-    <a href="index.htm#" class="prev page-numbers"><i class="icofont-simple-right"></i></a>
-    <a href="index.htm#" class="page-numbers">1</a> <span class="page-numbers current" aria-current="page">2</span>
-    <a href="index.htm#" class="page-numbers">3</a>
-    <a href="index.htm#" class="next page-numbers"><i class="icofont-simple-left"></i></a>
-    <a href="index.htm#" class="next page-numbers"><i class="icofont-double-left"></i></a>
-</div>
+@if ($paginator->hasPages())
+    <div class="pagination-area">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <span class="prev page-numbers"><i class="icofont-simple-right"></i></span>
+        @else
+            <a href="{{ $paginator->previousPageUrl() }}" class="prev page-numbers"><i class="icofont-simple-right"></i></a>
+        @endif
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <span class="page-numbers current" aria-current="page">{{ $element }}</span>
+            @endif
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <span class="page-numbers current" aria-current="page">{{ $page }}</span>
+                    @else
+                        <a href="{{ $url }}" class="page-numbers">{{ $page }}</a>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a href="{{ $paginator->nextPageUrl() }}" class="next page-numbers"><i class="icofont-simple-left"></i></a>
+        @else
+            <span class="next page-numbers"><i class="icofont-simple-left"></i></span>
+        @endif
+    </div>
+@endif
