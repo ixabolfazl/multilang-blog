817ca1fb889160fdfe8c9e54f93bd1b4621eb5f6
Abolfazl <kabolfazl39@gmail.com>
2021-08-06 09:52:51 +0430

    complate app/footer

--- resources/views/app/layouts/footer.blade.php
+++ resources/views/app/layouts/footer.blade.php
@@ -1,97 +1,18 @@
-
 <footer class="footer-area">
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-6">
                 <div class="single-footer-widget">
-                    <h3>درباره مجله خبری</h3>
+                    <h3>{{ __('About') }}</h3>
                     <div class="contact-info">
-                        <p>لورم ایپسوم به سادگی ساختار چاپ و متن را در بر می گیرد. لورم ایپسوم به مدت 40 سال استاندارد
-                            صنعت بوده است. لورم ایپسوم به سادگی ساختار چاپ و متن است.</p>
+                        <p>{{ $setting[app()->getLocale()]->about }}</p>
                         <ul>
-                            <li><i class="icofont-google-map"></i> کشور شما ، استان و شهر شما ، محله سکونت</li>
-                            <li><i class="icofont-phone"></i> <a href="index.htm#">021-12345678</a></li>
-                            <li><i class="icofont-envelope"></i> <a href="index.htm#"><span class="__cf_email__"
-                                                                                            data-cfemail="94fdfaf2fbd4e7fdfaf9e1fabaf7fbf9">[email&#160;protected]</span></a>
+                            <li><i class="icofont-google-map"></i>{{ $setting[app()->getLocale()]->address }}</li>
+                            <li><i class="icofont-phone"></i>{{ $setting[app()->getLocale()]->phone_number }}</li>
+                            <li><i class="icofont-envelope"></i>
+                                <span class="__cf_email__" data-cfemail="94fdfaf2fbd4e7fdfaf9e1fabaf7fbf9">{{ $setting[app()->getLocale()]->email }}</span>
                             </li>
                         </ul>
-                    </div>
-                    <div class="connect-social">
-                        <p>ما را دنبال کنید ، با ما در ارتباط باشید:</p>
-                        <ul>
-                            @include('app.layouts.social')
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6">
-                <div class="single-footer-widget">
-                    <h3>اخبار وبلاگ</h3>
-                    <div class="footer-latest-news-list">
-                        <div class="media latest-news-media align-items-center">
-                            <a href="index.htm#">
-                                <img src="{{ asset('assets/app/img/small-2.jpg') }}" alt="image">
-                            </a>
-                            <div class="content">
-                                <ul>
-                                    <li><i class="icofont-calendar"></i> 22 دی 1398</li>
-                                    <li><a href="index.htm#"><i class="icofont-comment"></i> 12</a></li>
-                                </ul>
-                                <h3><a href="index.htm#">لورم ایپسوم متن ساختگی با تولید نامفهوم است.</a></h3>
-                            </div>
-                        </div>
-                        <div class="media latest-news-media align-items-center">
-                            <a href="index.htm#">
-                                <img src="{{ asset('assets/app/img/small-4.jpg') }}" alt="image">
-                            </a>
-                            <div class="content">
-                                <ul>
-                                    <li><i class="icofont-calendar"></i> 22 دی 1398</li>
-                                    <li><a href="index.htm#"><i class="icofont-comment"></i> 12</a></li>
-                                </ul>
-                                <h3><a href="index.htm#">لورم ایپسوم متن ساختگی با تولید نامفهوم است.</a></h3>
-                            </div>
-                        </div>
-                        <div class="media latest-news-media align-items-center">
-                            <a href="index.htm#">
-                                <img src="{{ asset('assets/app/img/small-3.jpg') }}" alt="image">
-                            </a>
-                            <div class="content">
-                                <ul>
-                                    <li><i class="icofont-calendar"></i> 22 دی 1398</li>
-                                    <li><a href="index.htm#"><i class="icofont-comment"></i> 12</a></li>
-                                </ul>
-                                <h3><a href="index.htm#">لورم ایپسوم متن ساختگی با تولید نامفهوم است.</a></h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 offset-lg-0 offset-md-3">
-                <div class="single-footer-widget">
-                    <h3>خوراک توئیتر</h3>
-                    <div class="twitter-tweet-list">
-                        <div class="single-tweet">
-                            <i class="icofont-twitter"></i>
-                            <span>2 روز پیش</span>
-                            <p>کنفرانس رویداد تم وردپرس -> 2 خانه جدید اضافه شده # سخنرانی # کلمه # هدف # فدراسیون
-                                کلیساها # موضوع وردپرس <a href="index.htm#">#پوسته وردپرس
-                                    https://tt.co/Rn00zM5q7gY70</a></p>
-                        </div>
-                        <div class="single-tweet">
-                            <i class="icofont-twitter"></i>
-                            <span>2 روز پیش</span>
-                            <p>کنفرانس رویداد تم وردپرس -> 2 خانه جدید اضافه شده # سخنرانی # کلمه # هدف # فدراسیون
-                                کلیساها # موضوع وردپرس <a href="index.htm#">#پوسته وردپرس
-                                    https://tt.co/Rn00zM5q7gY70</a></p>
-                        </div>
-                        <div class="single-tweet">
-                            <i class="icofont-twitter"></i>
-                            <span>2 روز پیش</span>
-                            <p>کنفرانس رویداد تم وردپرس -> 2 خانه جدید اضافه شده # سخنرانی # کلمه # هدف # فدراسیون
-                                کلیساها # موضوع وردپرس <a href="index.htm#">#پوسته وردپرس
-                                    https://tt.co/Rn00zM5q7gY70</a></p>
-                        </div>
                     </div>
                 </div>
             </div>
@@ -101,11 +22,7 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 col-md-12">
-                    <p>کپی رایت © 1398 .تمام حقوق قالب محفوظ است. طراحی و توسعه توسط <a href="#" target="_blank">Barat
-                            Hadian</a></p>
-                </div>
-                <div class="col-lg-6 col-md-12">
-                    @include('app.layouts.footer-menu')
+                    <p>تمامی حقوق محفوظ است.</p>
                 </div>
             </div>
         </div>
